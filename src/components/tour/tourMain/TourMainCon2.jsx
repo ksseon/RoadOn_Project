575cc89378d62ca448ae3f1fd82c9f2352050c11
Your Name <viggitore@gmail.com>
2025-09-11 10:38:48 +0900

    0911 KHY 01
    button ui

--- src/components/tour/tourMain/TourMainCon2.jsx
+++ src/components/tour/tourMain/TourMainCon2.jsx
@@ -3,10 +3,10 @@ import './style.scss';
 const TourMainCon2 = () => {
     return (
         <section className="tour-main-con tour-main-con2">
-            {/* <div className="head-txt-box">
-                <h2>씬투어 , OTT 속 그 장면을 여행으로, </h2>
-                <p>화면 속 순간을 현실 여행으로 이어드립니다.</p>
-            </div> */}
+            <button className="button large g">large</button>
+            <button className="button middle g">middle</button>
+            <button className="button small o">small</button>
+            <button className="button Xsmall o2 ">Xsmall</button>
         </section>
     );
 };
