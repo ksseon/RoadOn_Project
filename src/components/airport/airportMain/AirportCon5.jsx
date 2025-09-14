01d16577209fce87d96da70b94147c4210c95dc4
ksseon <rtweq35@gmail.com>
2025-09-14 19:04:21 +0900

    항공 검색창 컴포넌트

--- src/components/airport/airportMain/AirportCon5.jsx
+++ src/components/airport/airportMain/AirportCon5.jsx
@@ -5,16 +5,25 @@ const AirportCon5 = () => {
         <h3>다양한 이벤트</h3>
 
         <div className="promo-list">
-          <div className="event1">
-            <p>해외에서 데이터 절약! 와이파이 & ESIM 4%할인</p>
+          <div className="event card1">
+            <p>
+              해외에서 데이터 절약!
+              <br /> 와이파이 & ESIM 4%할인
+            </p>
             <img src="/images/airport/event_img1.png" />
           </div>
-          <div className="event2">
-            <p>RoadOn 고객 여행자 보험 10% 즉시할인</p>
+          <div className="event card2">
+            <p>
+              RoadOn 고객
+              <br /> 여행자 보험 10% 즉시할인
+            </p>
             <img src="/images/airport/event_img2.png" />
           </div>
-          <div className="event3">
-            <p>카드사 혜택! 해외 항공권 할인</p>
+          <div className="event card3">
+            <p>
+              카드사 혜택!
+              <br /> 해외 항공권 할인
+            </p>
             <img src="/images/airport/event_img3.png" />
           </div>
         </div>
