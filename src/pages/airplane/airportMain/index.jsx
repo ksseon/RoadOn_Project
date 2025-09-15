31fd8f377ebcba1c539e06574959d25f3eaf5ffa
ksseon <rtweq35@gmail.com>
2025-09-15 22:16:45 +0900

    xc

---
+++ src/pages/airplane/airportMain/index.jsx
@@ -0,0 +1,11 @@
+import AirportMain from "../../../components/airport/airportMain";
+
+const Airport = () => {
+  return (
+    <main id="AirportPage">
+      <AirportMain />
+    </main>
+  );
+};
+
+export default Airport;
--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -1,36 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from '../common/Layout';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../common/Layout";
 import {
-    Airplane,
-    Home,
-    HotelsMain,
-    Tour,
-    MyPage,
-    NotFiles,
-    Login,
-    HotelsSearch,
-    HotelsDetail,
-    AirportSearch,
-} from '../pages';
+  AirportMain,
+  Home,
+  HotelsMain,
+  Tour,
+  MyPage,
+  NotFiles,
+  Login,
+  HotelsSearch,
+  HotelsDetail,
+  AirportSearch,
+} from "../pages";
 
 export const MyRoutes = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="/hotels" element={<HotelsMain />} />
-                    <Route path="/hotelsSearch" element={<HotelsSearch />} />
-                    {/* <Route path="/hotelsDetail" element={<HotelsDetail />} /> */}
-                    <Route path="/hotels/:slug" element={<HotelsDetail />} />
-                    <Route path="/airportSearch" element={<AirportSearch />} />
-                    <Route path="/tour" element={<Tour />} />
-                    <Route path="/airplane" element={<Airplane />} />
-                    <Route path="/myPage" element={<MyPage />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/*" element={<NotFiles />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/hotels" element={<HotelsMain />} />
+          <Route path="/hotelsSearch" element={<HotelsSearch />} />
+          {/* <Route path="/hotelsDetail" element={<HotelsDetail />} /> */}
+          <Route path="/hotels/:slug" element={<HotelsDetail />} />
+          <Route path="/airportSearch" element={<AirportSearch />} />
+          <Route path="/tour" element={<Tour />} />
+          <Route path="/airportMain" element={<AirportMain />} />
+          <Route path="/myPage" element={<MyPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<NotFiles />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
