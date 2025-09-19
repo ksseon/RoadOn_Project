d6bbb36faa8927cbafa54cfc7999b0e5080a5381
Hwanyeong815 <viggitore@gmail.com>
2025-09-19 16:41:08 +0900

    KHY 0919 main
    re

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,9 @@
 import Content1 from '../../components/home/content1/Content1';
 import Content2 from '../../components/home/content2/Content2';
 import Content3 from '../../components/home/content3/Content3';
-import Content4 from '../../components/home/content4/Content3';
-import Content5 from '../../components/home/content5/Content3';
-import Content6 from '../../components/home/content6/Content3';
+import Content4 from '../../components/home/content4/Content4';
+import Content5 from '../../components/home/content5/Content5';
+import Content6 from '../../components/home/content6/Content6';
 import HomeVisual from '../../components/home/visual/HomeVisual';
 import './style.scss';
 
@@ -11,12 +11,12 @@ function Home() {
     return (
         <main className="home">
             <HomeVisual />
-            <Content1 />
-            <Content2 />
-            <Content3 />
+            {/* <Content1 /> */}
+            {/* <Content2 /> */}
+            {/* <Content3 /> */}
             <Content4 />
-            <Content5 />
-            <Content6 />
+            {/* <Content5 /> */}
+            {/* <Content6 /> */}
         </main>
     );
 }
